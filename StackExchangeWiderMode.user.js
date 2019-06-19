fe873dc94ddbeda5e503540cd2bcce5d39c04170
Samuel Liew <samliew@gmail.com>
2019-06-19 12:23:21 +1000

    Update StackExchangeWiderMode.user.js

--- StackExchangeWiderMode.user.js
+++ StackExchangeWiderMode.user.js
@@ -3,7 +3,10 @@
 // @description  Increase max-width of sites to 1440px
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.0.5
+// @version      1.0.6
+//
+// @updateURL    https://github.com/samliew/SO-mod-userscripts/raw/master/StackExchangeWiderMode.user.js
+// @downloadURL  https://github.com/samliew/SO-mod-userscripts/raw/master/StackExchangeWiderMode.user.js
 //
 // @include      https://*stackexchange.com/*
 // @include      https://*stackoverflow.com/*
