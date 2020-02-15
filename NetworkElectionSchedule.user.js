86e58d9421175f64a08edd085bb21f3e36dc4b12
Samuel Liew <samliew@gmail.com>
2020-02-15 16:05:04 +0800

    Bugfix

--- NetworkElectionSchedule.user.js
+++ NetworkElectionSchedule.user.js
@@ -3,7 +3,7 @@
 // @description  Displays a list of upcoming and ongoing elections on https://stackexchange.com/elections
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      0.3.8
+// @version      0.4
 //
 // @include      https://stackexchange.com/elections
 //
@@ -163,7 +163,7 @@
             }
 
             // Simple throttle
-            if(ajaxCount > 15) {
+            if(ajaxCount > 20) {
 
                 // Refresh page after a minute
                 setTimeout(() => location.reload(), 60000);
@@ -180,9 +180,14 @@
             ajaxPromise(site.site_url + '/election' + (next == 0 ? '' : '/' + next), 'html').then(function(data) {
 
                 const html = $($.parseHTML(data));
+                const htmlStr = data.documentElement ? data.documentElement.outerHTML : data.outerHTML;
+
+                if(typeof html === 'undefined' || typeof htmlStr === 'undefined') {
+                    return;
+                }
 
                 // Check if election listing page (no active election)
-                if(data.includes('Community Moderator Elections') && data.includes('There are no active community moderator elections') || html.find('#mainbar-full > p').length == 1) {
+                if(htmlStr.includes('Community Moderator Elections') && htmlStr.includes('There are no active community moderator elections') || html.find('#mainbar-full > p').length == 1) {
                     const lastElectionLink = $('a[href*="/election/"]', html).last();
                     let lastElection = null, lastElectionEndDate = null;
 
@@ -255,7 +260,7 @@
 
     function displaySiteOngoingElection(site, electionNum, nomination, primary, election, endDate, candidates, seats) {
         electionItems.prepend(`<tr class="active-election" data-timestamp="${new Date(endDate).getTime() || Date.now()}">
-  <td><img src="${site.icon_url}" class="siteicon" /></td>
+  <td><img data-src="${site.icon_url}" class="siteicon" /></td>
   <td><a href="${site.site_url}/election/${electionNum}" target="_blank">${site.name}</a></td>
   <td><a href="${site.site_url}/election/${electionNum}?tab=nomination" target="_blank">${nomination}</a></td>
   <td><a href="${site.site_url}/election/${electionNum}?tab=primary" target="_blank">${primary ? primary : '-'}</a></td>
